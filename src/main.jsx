082506128b9d162ebd03b1a0ac9917cfbf437fea
noorjahan <noorjahan786.niet@gmail.com>
2025-08-21 10:06:25 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/footwear-ecommerce-website">
+  <BrowserRouter basename={process.env.NODE_ENV === "production" ? "/footwear-ecommerce-website" : "/"}>
   <ShopContextProvider>
 
   <App/>
