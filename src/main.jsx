6f179fa7bbd37ad04b02689a89926180fa0f4c88
noorjahan <noorjahan786.niet@gmail.com>
2025-08-21 10:19:54 +0530

    Fix homepage routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={process.env.NODE_ENV === "production" ? "/footwear-ecommerce-website" : "/"}>
+  <HashRouter >
   <ShopContextProvider>
 
   <App/>
   </ShopContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 )
